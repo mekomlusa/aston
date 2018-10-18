79fed22c1b48df177395838299dff2a0f87857a8
mekomlusa <meko.mlusa@gmail.com>
2018-10-18 15:14:27 -0500

    Add LSTM

--- codes/utilities/groundTruth.py
+++ codes/utilities/groundTruth.py
@@ -11,43 +11,55 @@ Sample usage:
     
     python groundTruth.py -p "D:\Google Drive\CSCE 638\project\samples"
     // Extracting stories located under D:\Google Drive\CSCE 638\project\samples
-    and save the summaries in the same directory.
+    and save the summaries and the texts in the same directory.
     
     python groundTruth.py -p "D:\Google Drive\CSCE 638\project\samples" -o "D:\Google Drive\CSCE 638\project\summaries"
     // Extracting stories located under D:\Google Drive\CSCE 638\project\samples
     and save the summaries in D:\Google Drive\CSCE 638\project\summaries
+    
+Python 3 rewrite
 """
 
 import re
 import os
 import argparse
 
-def summaryExtractor(storyLocation, outputLocation=None):
+def summaryExtractor(storyLocation, outputLocation=None, textLocation=None):
     rawSamples = os.listdir('%s' % storyLocation)
-    if storyLocation[:-1] != '\\':
-        storyLocation += '\\'
+    if storyLocation[:-1] != '/':
+        storyLocation += '/'
     
     for sample in rawSamples:
         with open(storyLocation+sample, 'r') as rf:
             data = rf.read()
             # first count how many @highlights
             sentenceCount = len(re.findall(r'@highlight',data)) * -1
-            # get the summary
+            # get the summary and the original text
+            text = data.split('@highlight')[:sentenceCount][0]
+            text = text.replace('\n',' ')
             sumCollect = data.split('@highlight')[sentenceCount:]
             summary =  ". ".join([s.strip('\n') for s in sumCollect])
             summary += "."
             
         # write out
         sampleOutput = sample+".summary.txt"
-        if not outputLocation:
-            with open(storyLocation+sampleOutput, 'wb') as wf:
+        textOutput = sample+".text.txt"
+        if not outputLocation and textLocation:
+            with open(storyLocation+sampleOutput, 'w') as wf:
                 wf.write(summary)
+            with open(storyLocation+textOutput, 'w') as wf:
+                wf.write(text)
+            
         else:
-            if outputLocation[:-1] != '\\':
-                outputLocation += '\\'
+            if outputLocation[-1] != '/':
+                outputLocation += '/'
+            if textLocation[-1] != '/':
+                textLocation += '/'
         
-            with open(outputLocation+sampleOutput, 'wb') as wf:
+            with open(outputLocation+sampleOutput, 'w') as wf:
                 wf.write(summary)
+            with open(textLocation+textOutput, 'w') as wf:
+                wf.write(text)
         
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -58,12 +70,17 @@ if __name__ == "__main__":
     parser.add_argument('-o','--output', required=False,
                         metavar="/path/to/dataset/",
                         help='Output directory to save the summaries')
+    parser.add_argument('-t','--text', required=False,
+                        metavar="/path/to/dataset/",
+                        help='Output directory to save the text (no summary)')
     args = parser.parse_args()
     
-    print "Extracting summaries."
-    if args.output:
-        summaryExtractor(args.path, args.output)
-        print "Summaries could be found under",args.output
+    print("Extracting summaries.")
+    if args.output and args.text:
+        summaryExtractor(args.path, args.output, args.text)
+        print("Summaries could be found under",args.output)
+        print("Cleaned text could be found under",args.text)
     else:
         summaryExtractor(args.path)
-        print "Summaries could be found under",args.path
+        print("Summaries could be found under",args.path)
+        print("Cleaned text could be found under",args.path)
