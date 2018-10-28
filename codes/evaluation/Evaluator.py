8e8d8822c9f26425f5bcbe0936d8ae2a8257e128
skin <tsjtux@gmail.com>
2018-10-28 14:31:05 -0500

    update evaluator

--- codes/evaluation/Evaluator.py
+++ codes/evaluation/Evaluator.py
@@ -1,6 +1,7 @@
-import numpy as np
 import re
-class evaluator:
+
+
+class Evaluator:
 
     def sample(self):
         ground_truth = ["The 15 new cardinals will be installed on February 14", 
@@ -11,18 +12,17 @@ class evaluator:
                 'Christopher Bellitto, a professor of church history at Kean University in New Jersey, noted that Francis announced his new slate of cardinals on the Catholic Feast of the Epiphany, which commemorates the visit of the Magi to Jesus\' birthplace in Bethlehem.', 
                 'Beginning in the 1920s, an increasing number of Latin American churchmen were named cardinals, and in the 1960s, St. John XXIII, whom Francis canonized last year, appointed the first cardinals from Japan, the Philippines and Africa.']
         print "rounge1"
-        [P, R, F] = self.rounge1(pred = predict, test = ground_truth )
+        [P, R, F] = self.rounge1(pred=predict, test=ground_truth )
         print "P: %s" % P
         print "R: %s" % R
         print "F-1: %s" % F
 
         print "rounge2"
-        [P, R, F] = self.rounge2(pred = predict, test = ground_truth )
+        [P, R, F] = self.rounge2(pred=predict, test=ground_truth )
         
         print "P: %s" % P
         print "R: %s" % R
         print "F-1: %s" % F
-
 
     def rounge2(self, test, pred):
         '''
@@ -33,18 +33,19 @@ class evaluator:
         p, r, f
         '''
         test_word_pair_dict, cnt_test_pair = self.count_word_pairs(test)
-        predWordPairDict, cnt_pred_pair = self.count_word_pairs(pred)
+        pred_word_pair_dict, cnt_pred_pair = self.count_word_pairs(pred)
 
-        # compare words in test and pred, if words exist in test and both in pred, we record this word, cnt++, cnts for all times the words appears instead of unique words
+        # compare words in test and pred, if words exist in test and both in pred, we record this word, cnt++, cnts for
+        # all times the words appears instead of unique words
         cnt_found_pair = 0
-        for p in predWordPairDict:
+        for p in pred_word_pair_dict:
             if p in test_word_pair_dict:
                 test_word_pair_dict[p] -= 1
                 if test_word_pair_dict[p] == 0:
                     test_word_pair_dict.pop(p)
                 cnt_found_pair += 1
 
-        #precision and recall and f1 score, 
+        # precision and recall and f1 score,
         P = float(cnt_found_pair) / float(cnt_pred_pair)
         R = float(cnt_found_pair) / float(cnt_test_pair)
         F = 2.0 * (P * R) / (P + R)
@@ -60,7 +61,7 @@ class evaluator:
         '''
         pairs = dict()
         cnt = 0
-        #  cnts for word pairs, for start words, marked as (*, word), for end words, marked as (word, #)
+        # cnts for word pairs, for start words, marked as (*, word), for end words, marked as (word, #)
         for sentence in sentences_list:
             word_list = re.split('\W+', sentence)
             cnt += 1 + len(word_list)
@@ -91,20 +92,21 @@ class evaluator:
         P, R, F
         '''
         from collections import Counter
-        #cnts for all words in test cases and ground truth cases
+        # cnts for all words in test cases and ground truth cases
         test_words = re.split('\W+', " ".join(test))
         pred_words = re.split('\W+', " ".join(pred))
         cnts_test = Counter()
         for word in test_words:
             cnts_test[word] += 1
-            test_dict = dict(cnts_test)
+        test_dict = dict(cnts_test)
 
         cnts_pred = Counter()
         for word in pred_words:
             cnts_pred[word] += 1
-            pred_dict = dict(cnts_pred)
+        pred_dict = dict(cnts_pred)
 
-        # compare words in test and pred, if words exist in test and both in pred, we record this word, cnt++, cnts for all times the words appears instead of unique words
+        # compare words in test and pred, if words exist in test and both in pred, we record this word, cnt++, cnts for
+        # all times the words appears instead of unique words
         cnt_extracted = 0
         for word in pred_dict:
             if word in test_dict:
@@ -113,13 +115,14 @@ class evaluator:
                     test_dict.pop(word)
                 cnt_extracted += 1
 
-        #precision and recall and f1 score, 
+        # precision and recall and f1 score,
         P = float(cnt_extracted) / float(len(pred_words))
         R = float(cnt_extracted) / float(len(test_words))
         F = 2.0 * (P * R) / (P + R)
 
         return [P, R, F]
 
+
 if __name__ == '__main__':
-    test = evaluator()
+    test = Evaluator()
     test.sample()
