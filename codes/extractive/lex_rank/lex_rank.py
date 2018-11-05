472bad2b0cc276c3774fa013edb227e4ec232ea0
skin <tsjtux@gmail.com>
2018-11-05 17:58:56 -0600

    implement word similarity

--- codes/extractive/lex_rank/lex_rank.py
+++ codes/extractive/lex_rank/lex_rank.py
@@ -7,7 +7,11 @@ from codes.evaluation.Evaluator import Evaluator
 from heapq import heappush, heappop
 from nltk.corpus import stopwords
 from nltk.tokenize import sent_tokenize
+from nltk.corpus import wordnet as wn
 from nltk.stem import PorterStemmer, WordNetLemmatizer
+from sumy.summarizers.lex_rank import LexRankSummarizer
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import Tokenizer
 import io
 import math
 import numpy as np
@@ -16,11 +20,12 @@ import re
 
 
 STOP_WORDS = None
-DAMPING = 0.5
+DAMPING = 0.85
 COS_THRESHOLD = 0.1
-EIGEN_VEC_MAX_ERR = 0.05
+EIGEN_VEC_MAX_ERR = 0.001
 STEMMER = PorterStemmer()
 LEMMATIZER = WordNetLemmatizer()
+USE_SIMILARITY = True
 
 
 def idf_modified_cosine(s1, s2, idf_dict, stem_dict, choice):
@@ -71,6 +76,37 @@ def idf_modified_cosine(s1, s2, idf_dict, stem_dict, choice):
     return res * 1.0 / math.sqrt(tf_idf_sum1) * math.sqrt(tf_idf_sum2)
 
 
+def sentence_similarity(s1, s2):
+    ws1 = []
+    ws2 = []
+    for w in re.findall(r'[a-zA-Z]+', s1):
+        if w in STOP_WORDS or len(w) < WORD_MIN_LEN:
+            continue
+        senses = wn.synsets(w)
+        if len(senses) == 0:
+            continue
+        ws1.append(senses[0])
+
+    for w in re.findall(r'[a-zA-Z]+', s2):
+        if w in STOP_WORDS or len(w) < WORD_MIN_LEN:
+            continue
+        senses = wn.synsets(w)
+        if len(senses) == 0:
+            continue
+        ws2.append(senses[0])
+
+    similarity = 0.0
+    total = 0.0
+    for sense1 in ws1:
+        for sense2 in ws2:
+            total += 1.0
+            cur_sim = wn.path_similarity(sense1, sense2)
+            if cur_sim:
+                similarity += cur_sim
+
+    return similarity / total
+
+
 def power_method(cos_mat, num_sents, err):
     p = np.ones((num_sents, 1))
     p /= 1.0 * num_sents
@@ -93,7 +129,7 @@ def calc_lex_rank_scores(sents, idf_dict, is_damped=False, choice=0):
     stem_dict = dict()
     for i in range(0, num_sents):
         for j in range(0, num_sents):
-            cos_mat[i][j] = idf_modified_cosine(sents[i], sents[j], idf_dict, stem_dict, choice)
+            cos_mat[i][j] = sentence_similarity(sents[i], sents[j]) if USE_SIMILARITY else (sents[i], sents[j], idf_dict, stem_dict, choice)
             if cos_mat[i][j] > COS_THRESHOLD:
                 cos_mat[i][j] = 1.0
                 deg[i] += 1
@@ -184,6 +220,75 @@ def test_evaluation_batch(stories_dir_path, num_files, idf_dict, is_damped=False
     print('Avg. F-1 of {0} samples in rounge 2: {1}'.format(num_files, global_f2 / num_files))
 
 
+def summy_lex_rank_process_article_file(file_path):
+    sents = []
+    with io.open(file_path, 'r', encoding='utf-8') as article_file:
+        for line in article_file:
+            if line.find('@highlight') != -1:
+                break
+            line = line.strip()
+            # skip subtitles
+            if len(line) == 0 or line[-1].isalnum():
+                continue
+            cur_sents = sent_tokenize(line)
+            for s in cur_sents:
+                if len(s) >= MIN_NUM_WORDS_IN_SENT:
+                    sents.append(s)
+    parser = PlaintextParser.from_string(' '.join(sents), Tokenizer('english'))
+    summarizer = LexRankSummarizer()
+    # Summarize the document with 2 sentences
+    sums = summarizer(parser.document, NUM_SUM_SENTS)
+    res_list = []
+    for summary in sums:
+        res_list.append(str(summary))
+    return res_list
+
+
+def test_sumy_lexrank_pack_batch(stories_dir_path, num_files):
+    count = 0
+    ev = Evaluator()
+    global_p1 = 0.0
+    global_r1 = 0.0
+    global_f1 = 0.0
+    global_p2 = 0.0
+    global_r2 = 0.0
+    global_f2 = 0.0
+    for story_file in os.listdir(stories_dir_path):
+        story_file_path = os.path.join(stories_dir_path, story_file)
+        cur_sums = summy_lex_rank_process_article_file(story_file_path)
+        ground_truth_sums = get_ground_truth_sum(story_file_path)
+        [p1, r1, f1] = ev.rounge1(cur_sums, ground_truth_sums)
+        [p2, r2, f2] = ev.rounge2(cur_sums, ground_truth_sums)
+        count += 1
+        global_p1 += p1
+        global_r1 += r1
+        global_f1 += f1
+        global_p2 += p2
+        global_r2 += r2
+        global_f2 += f2
+        if count == num_files:
+            break
+
+    print('rouge 1 results')
+    print('Avg. P of {0} samples in rounge 1: {1}'.format(num_files, global_p1 / num_files))
+    print('Avg. R of {0} samples in rounge 1: {1}'.format(num_files, global_r1 / num_files))
+    print('Avg. F-1 of {0} samples in rounge 1: {1}'.format(num_files, global_f1 / num_files))
+    print('rouge 2 results')
+    print('Avg. P of {0} samples in rounge 2: {1}'.format(num_files, global_p2 / num_files))
+    print('Avg. R of {0} samples in rounge 2: {1}'.format(num_files, global_r2 / num_files))
+    print('Avg. F-1 of {0} samples in rounge 2: {1}'.format(num_files, global_f2 / num_files))
+
+
+def test_word_similarity():
+    s1 = 'No Americans made the list this time or the previous time in Francis'
+    s2 = 'Pope Francis said Sunday that he would hold a meeting of cardinals on February 14'
+    s3 = 'Beginning in the 1920s, an increasing number of Latin American churchmen were named cardinals, and in the 196' \
+         '0s, St. John XXIII, whom Francis canonized last year, appointed the first cardinals from Japan, the Philippin' \
+         'es and Africa.'
+    print sentence_similarity(s1, s2)
+    print sentence_similarity(s1, s3)
+
+
 if __name__ == '__main__':
     idf_dict_original = calc_idf()
     idf_dict_original_bi = calc_idf(True)
@@ -193,18 +298,26 @@ if __name__ == '__main__':
     dir_name = os.path.dirname(os.path.abspath(__file__))
     root_dir = os.path.abspath(os.path.join(dir_name, os.pardir, os.pardir, os.pardir))
     stories_dir = os.path.join(root_dir, 'cnn_stories')
-    # story_file_path = os.path.join(stories_dir, '0a0adc84ccbf9414613e145a3795dccc4828ddd4.story')
-    # test_evaluation(story_file_path)
-    num_test_files = 100
-    print('original')
-    test_evaluation_batch(stories_dir, num_test_files, idf_dict_original)
+
+    num_test_files = 5
+    # print('original')
+    # test_evaluation_batch(stories_dir, num_test_files, idf_dict_original)
     print('\ndamped')
     test_evaluation_batch(stories_dir, num_test_files, idf_dict_original, is_damped=True)
-    print('\nbinary')
-    test_evaluation_batch(stories_dir, num_test_files, idf_dict_original_bi)
-    print('\nstemming')
-    test_evaluation_batch(stories_dir, num_test_files, idf_dict_stem, choice=1)
-    print('\nlemma')
-    test_evaluation_batch(stories_dir, num_test_files, idf_dict_lemma, choice=2)
+    print('\nsumy lexrank')
+    test_sumy_lexrank_pack_batch(stories_dir, num_test_files)
+    # for i in range(10):
+    #     DAMPING = 0.1 * i
+    #     print('Damping factor: {0}'.format(DAMPING))
+    #     test_evaluation_batch(stories_dir, num_test_files, idf_dict_original, is_damped=True)
+    # print('\nbinary')
+    # test_evaluation_batch(stories_dir, num_test_files, idf_dict_original_bi)
+    # print('\nstemming')
+    # test_evaluation_batch(stories_dir, num_test_files, idf_dict_stem, choice=1)
+    # print('\nlemma')
+    # test_evaluation_batch(stories_dir, num_test_files, idf_dict_lemma, choice=2)
+
+    # test_word_similarity()
+
 
 
